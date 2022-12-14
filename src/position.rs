cf0a592f5985617f0bbed7530c83f01a55f3c71d
Jacques <jamie.whiting2003@gmail.com>
2022-12-14 00:14:11 +0000

    simplified do_move

--- src/position.rs
+++ src/position.rs
@@ -51,8 +51,7 @@ impl Pos {
     pub fn is_sq_att(&self, idx: usize, side: usize, occ: u64) -> bool {
         let s: u64 = self.s[side ^ 1];
         let opp_queen: u64 = self.pc[Q] & s;
-        (NATT[idx] & self.pc[N] & s > 0)
-        || (KATT[idx] & self.pc[K] & s > 0)
+        (NATT[idx] & self.pc[N] & s > 0) || (KATT[idx] & self.pc[K] & s > 0)
         || (PATT[side][idx] & self.pc[P] & s > 0)
         || (ratt(idx, occ) & (self.pc[R] & s | opp_queen) > 0)
         || (batt(idx, occ) & (self.pc[B] & s | opp_queen) > 0)
@@ -74,29 +73,20 @@ impl Pos {
 
         self.toggle(self.c, mpc, f | t);
         self.state.enp = 0;
-        if cpc != E { self.toggle(opp, cpc, t); }
-        if cpc == R { self.state.cr &= CR[m.to as usize]; }
-        match mpc {
-            P => {
-                if m.flag == ENP {
-                    let p: u64 = if opp == WH {t << 8} else {t >> 8};
-                    self.toggle(opp, P, p);
-                } else if m.flag == DBL {
-                    self.state.enp = if opp == BL {m.to - 8} else {m.to + 8};
-                } else if m.flag >= PROMO {
-                    let ppc: u8 = (m.flag & 3) + 1;
-                    self.pc[mpc] ^= t;
-                    self.pc[ppc as usize] ^= t;
-                }
+        if cpc != E {
+            self.toggle(opp, cpc, t);
+            if cpc == R { self.state.cr &= CR[m.to as usize]; }
+        }
+        if mpc == R || mpc == K { self.state.cr &= CR[m.from as usize] }
+        match m.flag {
+            ENP => self.toggle(opp, P, if opp == WH {t << 8} else {t >> 8}),
+            DBL => self.state.enp = if opp == BL {m.to - 8} else {m.to + 8},
+            KS => self.toggle(self.c, R, CKM[self.c]),
+            QS => self.toggle(self.c, R, CQM[self.c]),
+            PROMO.. => {
+                self.pc[mpc] ^= t;
+                self.pc[((m.flag & 3) + 1) as usize] ^= t;
             }
-            K => {
-                self.state.cr &= CR[m.from as usize];
-                if m.flag == KS || m.flag == QS {
-                    let c: u64 = CASTLE_MOVES[self.c][(m.flag == KS) as usize];
-                    self.toggle(self.c, R, c);
-                }
-            }
-            R => self.state.cr &= CR[m.from as usize],
             _ => {}
         }
         self.state.hfm = if mpc > P && cpc != E {0} else {self.state.hfm + 1};
