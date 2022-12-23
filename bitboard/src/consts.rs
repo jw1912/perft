427dc2e0a5b5acc916b146d1110bf6e9aebbe344
Jacques <jamie.whiting2003@gmail.com>
2022-12-23 20:32:13 +0000

    improved init macro

--- bitboard/src/consts.rs
+++ bitboard/src/consts.rs
@@ -1,10 +1,10 @@
-// macro for calculating tables (until const fn pointers are stable)
+/// Macro for calculating tables (until const fn pointers are stable).
 macro_rules! init {
-    ($init:stmt, $idx:expr, $initial:expr, $func:expr) => {{
-        let mut res = [$initial; 64];
-        $init
+    ($idx:ident, $init:expr, $($rest:tt)+) => {{
+        let mut res = [$init; 64];
+        let mut $idx = 0;
         while $idx < 64 {
-            res[$idx] = $func;
+            res[$idx] = {$($rest)+};
             $idx += 1;
         }
         res
@@ -34,7 +34,7 @@ pub const   F1G1: u64 = 0x0000000000000060;
 pub const B8C8D8: u64 = 0x0E00000000000000;
 pub const   F8G8: u64 = 0x6000000000000000;
 pub const CS: [u8; 2] = [WKS | WQS, BKS | BQS];
-pub const CR: [u8; 64] = init!(let mut idx = 0, idx, 0, match idx {0 => 7, 4 => 3, 7 => 11, 56 => 13, 60 => 12, 63 => 14, _ => 15});
+pub const CR: [u8; 64] = init!(idx, 0, match idx {0 => 7, 4 => 3, 7 => 11, 56 => 13, 60 => 12, 63 => 14, _ => 15});
 
 // for promotions / double pushes
 pub const PENRANK: [u64; 2] = [0x00FF000000000000, 0x000000000000FF00];
@@ -45,16 +45,16 @@ pub const FILE: u64 = 0x0101010101010101;
 pub const NOTH: u64 = !(FILE << 7);
 
 // rook attacks on rank
-pub const WEST: [u64; 64] = init!(let mut idx = 0, idx, 0, ((1 << idx) - 1) & (0xFF << (idx & 56)));
+pub const WEST: [u64; 64] = init!(idx, 0, ((1 << idx) - 1) & (0xFF << (idx & 56)));
 
 // pawn attacks
 pub const PATT: [[u64; 64]; 2] = [
-    init!(let mut idx = 0, idx, 0, (((1 << idx) & !FILE) << 7) | (((1 << idx) & NOTH) << 9)),
-    init!(let mut idx = 0, idx, 0, (((1 << idx) & !FILE) >> 9) | (((1 << idx) & NOTH) >> 7)),
+    init!(idx, 0, (((1 << idx) & !FILE) << 7) | (((1 << idx) & NOTH) << 9)),
+    init!(idx, 0, (((1 << idx) & !FILE) >> 9) | (((1 << idx) & NOTH) >> 7)),
 ];
 
 // knight attacks
-pub const NATT: [u64; 64] = init!(let mut idx = 0, idx, 0, {
+pub const NATT: [u64; 64] = init!(idx, 0, {
     let n = 1 << idx;
     let h1 = ((n >> 1) & 0x7f7f7f7f7f7f7f7f) | ((n << 1) & 0xfefefefefefefefe);
     let h2 = ((n >> 2) & 0x3f3f3f3f3f3f3f3f) | ((n << 2) & 0xfcfcfcfcfcfcfcfc);
@@ -62,7 +62,7 @@ pub const NATT: [u64; 64] = init!(let mut idx = 0, idx, 0, {
 });
 
 // king attacks
-pub const KATT: [u64; 64] = init!(let mut idx = 0, idx, 0, {
+pub const KATT: [u64; 64] = init!(idx, 0, {
     let mut k = 1 << idx;
     k |= (k << 8) | (k >> 8);
     k |= ((k & !FILE) >> 1) | ((k & NOTH) << 1);
@@ -77,16 +77,16 @@ pub const DIAGS: [u64; 15] = [
 ];
 
 // masks for hyperbola quintessence rook and bishop attacks
-pub const BMASKS: [Mask; 64] = init!(let mut idx = 0, idx, Mask { bit: 0, right: 0, left: 0, file: 0 }, {
+pub const BMASKS: [Mask; 64] = init!(idx, Mask { bit: 0, right: 0, left: 0, file: 0 },
     let bit = 1 << idx;
     Mask { bit, right: bit ^ DIAGS[(7 + (idx & 7) - (idx >> 3))], left: bit ^ DIAGS[((idx & 7) + (idx >> 3))].swap_bytes(), file: bit.swap_bytes() }
-});
+);
 
-pub const RMASKS: [Mask; 64] = init!(let mut idx = 0, idx, Mask { bit: 0, right: 0, left: 0, file: 0 }, {
+pub const RMASKS: [Mask; 64] = init!(idx, Mask { bit: 0, right: 0, left: 0, file: 0 },
     let bit = 1 << idx;
     let left = (bit - 1) & (0xFF << (idx & 56));
     Mask { bit, right: bit ^ left ^ (0xFF << (idx & 56)), left, file: bit ^ FILE << (idx & 7) }
-});
+);
 
 #[derive(Clone, Copy)]
 pub struct Mask {
